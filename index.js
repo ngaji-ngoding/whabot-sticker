bf5e45e792f6a9c50a8810d4fe2e42d87a6e21a7
fathb <eglass.boy@gmail.com>
2022-12-09 10:22:30 +0700

    message wc2022

--- index.js
+++ index.js
@@ -1,91 +1,97 @@
 const fs = require("fs");
-const {writeExifImg}= require("./lib/exif.js");
-const config = require("./config/config.json");
-const scheduledMsg = require("./config/scheduledMsg.json");
-const schedule = require("node-schedule");
+const { writeExifImg } = require("./lib/exif.js");
+let config = require("./config/config.json");
+//const scheduledMsg = require("./config/scheduledMsg.json");
+//const schedule = require("node-schedule");
 
 const {
   default: botSticker,
-    useSingleFileAuthState,
-    downloadMediaMessage,
-    DisconnectReason
-  } = require("@adiwajshing/baileys");
+  useSingleFileAuthState,
+  downloadMediaMessage,
+  DisconnectReason,
+} = require("@adiwajshing/baileys");
 
-  const {
-    Boom
-  } = require("@hapi/boom");
-  const P = require("pino");
-  
+const { Boom } = require("@hapi/boom");
+const P = require("pino");
 
-  const {
-    state,
-    saveState
-  } = useSingleFileAuthState("botStickerSession.json");
+const { state, saveState } = useSingleFileAuthState("botStickerSession.json");
 
-  const logger = P();
+const logger = P();
 
-  function runBot() {
-    const sock = botSticker({
-      auth: state,
-      printQRInTerminal: true,
-      logger: P({
-        level: "silent"
-      })
-    });
+function runBot() {
+  const sock = botSticker({
+    auth: state,
+    printQRInTerminal: true,
+    logger: P({
+      level: "silent",
+    }),
+  });
 
-    sock.ev.on("connection.update", ({
-      connection, lastDisconnect
-    })=> {
-      if (connection === "close") {
-        const error = new Boom(lastDisconnect.error);
-        const alasanError = error?.output?.statusCode;
+  sock.ev.on("connection.update", ({ connection, lastDisconnect }) => {
+    if (connection === "close") {
+      const error = new Boom(lastDisconnect.error);
+      const alasanError = error?.output?.statusCode;
 
-        if (alasanError === DisconnectReason.loggedOut) {
-          sock.logout();
-        } else {
-          runBot();
-        }
-
+      if (alasanError === DisconnectReason.loggedOut) {
+        sock.logout();
       } else {
-
-        console.log("connection opened");
+        runBot();
       }
-    });
+    } else {
+      console.log("connection opened");
+    }
+  });
 
-    sock.ev.on("messages.upsert",
-      async ({
-        messages, type
-      })=> {
-        const msg = messages[0];
-        if (!msg.message || msg.key.remoteJid === "status@broadcast" || msg.key.fromMe||!msg.message.imageMessage)return;
-if(msg.key.remoteJid === config.idGroup){
-        let caption = msg.message.imageMessage.caption;
+  sock.ev.on("messages.upsert", async ({ messages, type }) => {
+    const msg = messages[0];
+    if (
+      !msg.message ||
+      msg.key.remoteJid === "status@broadcast" ||
+      msg.key.fromMe
+    )
+      return;
+    if (msg.key.remoteJid === config.idGroup) {
+      let pesan;
+      //fitur sticker
+      if (msg.message.imageMessage) {
+        pesan = msg.message.imageMessage.caption;
+        if (pesan === "buatkan sticker") {
+          let buffer = await downloadMediaMessage(
+            msg,
+            "buffer",
+            {},
+            { logger }
+          );
 
-        let buffer = await downloadMediaMessage(msg, "buffer", {}, {
-          logger
-        });
-        
-        buffer = await writeExifImg(buffer, {packname:"ngaji ngoding", author:"ngaji ngoding"});
-        
-        if (caption === 'buatkan sticker') {
-          sock.sendMessage(msg.key.remoteJid, {sticker:{url: buffer}});
+          buffer = await writeExifImg(buffer, {
+            packname: "ngaji ngoding",
+            author: "ngaji ngoding",
+          });
+
+          sock.sendMessage(msg.key.remoteJid, { sticker: { url: buffer } });
           fs.unlinkSync(buffer);
         }
+      }
+      //pesan teks
+      if (msg.message.conversation) {
+        B;
+        pesan = msg.message.conversation;
+        if ((pesan = "wc2022")) {
+          console.log(pesan);
+        }
+      }
+    }
+  });
+
+  sock.ev.on("creds.update", saveState);
+
+  //scheduledMsg.forEach(el=>{
+  //  let date = new Date(el.date+"T"+el.time);
+  //  if(date == "Invalid Date") date = el.cron;
+  //  schedule.scheduleJob(date, ()=>{
+  //    sock.sendMessage(config.idGroup, {text: el.msg});
+  //  })
+  //})
 }
 
-      });
-
-    sock.ev.on("creds.update",
-      saveState);
-
-    scheduledMsg.forEach(el=>{
-      let date = new Date(el.date+"T"+el.time);
-      if(date == "Invalid Date") date = el.cron;
-      schedule.scheduleJob(date, ()=>{
-        sock.sendMessage(config.idGroup, {text: el.msg});
-      })
-    })
-  }
-
-
-  runBot();
+runBot();
